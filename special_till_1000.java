668ff78b58804df1bc6a6fb204cd536da4c2c5ff
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2023-10-17 19:11:49 +0530

    Update special_till_1000.java

--- special_till_1000.java
+++ special_till_1000.java
@@ -1,39 +1,30 @@
 package spiders;
 
 public class special_till_1000 {
-	
-	
 	public static void check(int n) {
 		int p=n;
 		int sum=0;
 		int mul=1;
 		int add=0;
 		int count=0;
-
-		while(n!=0) 
+		while(n!=0)
 		{
 			int digit=n%10;
-			sum=sum+digit;//0 0 8
-			mul=mul*digit;//00 0
-			add=mul+sum;//0 0 8
-			
-			n=n/10;
-			
+			sum=sum+digit;
+			mul=mul*digit;
+			add=mul+sum;
+			n=n/10;	
 		}
-		if(add==p) {
-			
+		if(add==p)
+		{
 			System.out.println(add+ " "+"special");
-		}
-		
+		}	
 	}
-	public static void main(String[] args) {
-		for(int i=1;i<=1000;i++) {
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=1000;i++)
+		{
 			check(i);
 		}
-		
 	}
-	
-	
-	
-	}
-
+}
