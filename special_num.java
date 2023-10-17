9fbc19d5da9a19d41f70444e05e46183282c6738
Om Jadhav <74755805+Omjadhav13@users.noreply.github.com>
2023-10-17 19:03:23 +0530

    Add files via upload

---
+++ special_num.java
@@ -0,0 +1,25 @@
+package spiders;
+
+public class special_num {
+	public static void main(String[] args) {
+		int n=19;
+		int p=n;
+		int sum=0;
+		int mul=1;
+		int add=0;
+		while(n!=0) {
+			int digit=n%10;
+			sum=sum+digit;//0 0 8
+			mul=mul*digit;//00 0
+			add=mul+sum;//0 0 8
+			n=n/10;
+		}
+		if(add==p) {
+			System.out.println("special");
+		}
+		else {
+			System.out.println("not a special");
+		}
+		
+	}
+}
